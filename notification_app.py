4867928c603fd12853a4c7ff322345ae1edd5711
Suresh L. Paul <sureshlazaruspaul@gmail.com>
2023-10-15 20:48:18 -0400

    add notification_app.py file

--- notification_app.py
+++ notification_app.py
@@ -1,15 +1,25 @@
-#import notification from plyer module
-from plyer import notification
-#import time
-import time
+"""
+Notification Reminder Script
+This script sends reminders to take a break at regular 
+intervals using system notifications.
 
-#Use while loop to create notifications indefinetly
-while(True):
-    #notification
+Requirements:
+- pip install plyer
+"""
+
+import time
+from plyer import notification
+
+# Set the number of notifications you want
+num_notifications = 10
+
+# Use a for loop to create notifications a specific number of times
+for _ in range(num_notifications):
+    # Notification
     notification.notify(
-        title = "Reminder to take a break",
-        message = "Drink water, take a walk",
-        timeout = 30
+        title="Reminder to take a break",
+        message="Drink water, take a walk",
+        timeout=30
     )
-    #System pause the execution of this programm for 60 minutes
+    # System pause the execution of this program for 60 minutes
     time.sleep(60)
